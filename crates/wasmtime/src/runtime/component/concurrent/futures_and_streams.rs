30da510711f136a0ef227613159bf6758fc124f6
Joel Dice <joel.dice@fermyon.com>
2025-11-03 17:31:59 -0700

    snapshot
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- crates/wasmtime/src/runtime/component/concurrent/futures_and_streams.rs
+++ crates/wasmtime/src/runtime/component/concurrent/futures_and_streams.rs
@@ -2221,78 +2221,78 @@ impl<T> StoreContextMut<'_, T> {
 
                     let (result, cancelled) = if buffer.remaining().is_empty() {
                         future::poll_fn(|cx| {
-                        tls::get(|store| {
-                            let transmit = store.concurrent_state_mut().get_mut(id).unwrap();
+                            tls::get(|store| {
+                                let transmit = store.concurrent_state_mut().get_mut(id).unwrap();
 
-                            let &WriteState::HostReady { cancel, .. } = &transmit.write else {
-                                unreachable!();
-                            };
-
-                            let mut host_buffer =
-                                if let ReadState::HostToHost { buffer, .. } = &mut transmit.read {
-                                    Some(Cursor::new(mem::take(buffer)))
-                                } else {
-                                    None
-                                };
-
-                            let poll = mine.as_mut().poll_produce(
-                                cx,
-                                token.as_context_mut(store),
-                                Destination {
-                                    id,
-                                    buffer: &mut buffer,
-                                    host_buffer: host_buffer.as_mut(),
-                                    _phantom: PhantomData,
-                                },
-                                cancel,
-                            );
-
-                            let transmit = store.concurrent_state_mut().get_mut(id).unwrap();
-
-                            let host_offset = if let (
-                                Some(host_buffer),
-                                ReadState::HostToHost { buffer, limit, .. },
-                            ) = (host_buffer, &mut transmit.read)
-                            {
-                                *limit = usize::try_from(host_buffer.position()).unwrap();
-                                *buffer = host_buffer.into_inner();
-                                *limit
-                            } else {
-                                0
-                            };
-
-                            {
-                                let WriteState::HostReady {
-                                    guest_offset,
-                                    cancel,
-                                    cancel_waker,
-                                    ..
-                                } = &mut transmit.write
-                                else {
+                                let &WriteState::HostReady { cancel, .. } = &transmit.write else {
                                     unreachable!();
                                 };
 
-                                if poll.is_pending() {
-                                    if !buffer.remaining().is_empty()
-                                        || *guest_offset > 0
-                                        || host_offset > 0
-                                    {
-                                        return Poll::Ready(Err(anyhow!(
-                                            "StreamProducer::poll_produce returned Poll::Pending \
-                                             after producing at least one item"
-                                        )));
-                                    }
-                                    *cancel_waker = Some(cx.waker().clone());
-                                } else {
-                                    *cancel_waker = None;
-                                    *cancel = false;
-                                }
-                            }
+                                let mut host_buffer =
+                                    if let ReadState::HostToHost { buffer, .. } = &mut transmit.read {
+                                        Some(Cursor::new(mem::take(buffer)))
+                                    } else {
+                                        None
+                                    };
 
-                            poll.map(|v| v.map(|result| (result, cancel)))
+                                let poll = mine.as_mut().poll_produce(
+                                    cx,
+                                    token.as_context_mut(store),
+                                    Destination {
+                                        id,
+                                        buffer: &mut buffer,
+                                        host_buffer: host_buffer.as_mut(),
+                                        _phantom: PhantomData,
+                                    },
+                                    cancel,
+                                );
+
+                                let transmit = store.concurrent_state_mut().get_mut(id).unwrap();
+
+                                let host_offset = if let (
+                                    Some(host_buffer),
+                                    ReadState::HostToHost { buffer, limit, .. },
+                                ) = (host_buffer, &mut transmit.read)
+                                {
+                                    *limit = usize::try_from(host_buffer.position()).unwrap();
+                                    *buffer = host_buffer.into_inner();
+                                    *limit
+                                } else {
+                                    0
+                                };
+
+                                {
+                                    let WriteState::HostReady {
+                                        guest_offset,
+                                        cancel,
+                                        cancel_waker,
+                                        ..
+                                    } = &mut transmit.write
+                                    else {
+                                        unreachable!();
+                                    };
+
+                                    if poll.is_pending() {
+                                        if !buffer.remaining().is_empty()
+                                            || *guest_offset > 0
+                                            || host_offset > 0
+                                        {
+                                            return Poll::Ready(Err(anyhow!(
+                                                "StreamProducer::poll_produce returned Poll::Pending \
+                                                 after producing at least one item"
+                                            )));
+                                        }
+                                        *cancel_waker = Some(cx.waker().clone());
+                                    } else {
+                                        *cancel_waker = None;
+                                        *cancel = false;
+                                    }
+                                }
+
+                                poll.map(|v| v.map(|result| (result, cancel)))
+                            })
                         })
-                    })
-                    .await?
+                            .await?
                     } else {
                         (StreamResult::Completed, false)
                     };
@@ -2340,10 +2340,16 @@ impl<T> StoreContextMut<'_, T> {
                     *producer.lock().unwrap() = Some((mine, buffer));
 
                     if write_buffer {
-                        write(instance, token, id, producer, kind).await?;
+                        write(instance, token, id, producer.clone(), kind).await?;
                     }
 
-                    Ok(result)
+                    Ok(if let StreamResult::Dropped = result
+                           && !producer.lock().unwrap().as_ref().unwrap().1.remaining().is_empty()
+                    {
+                        StreamResult::Completed
+                    } else {
+                        result
+                    })
                 }
                 .boxed()
             }
